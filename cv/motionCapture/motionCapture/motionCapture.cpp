4e285da02f9cc5f49d1aa0eac0d7070c845cec86
ZnHao203 <howeray203@gmail.com>
2024-10-04 08:28:37 -0400

    temporary drafts - not useful code

---
+++ cv/motionCapture/motionCapture/motionCapture.cpp
@@ -0,0 +1,35 @@
+// motionCapture.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <iostream>
+
+//int main()
+//{
+   /* std::cout << "Hello World!\n";
+}*/
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main()
+{
+    std::string image_path = "C:/Users/hower/Documents/akane/cv/capture/fishTurn.png";
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
